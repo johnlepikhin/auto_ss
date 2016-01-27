30b9ce7022b6ca72d6e19e5731bb4a5cce51d58c
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-01-27 22:48:15 +0300

    Init SexpLoc: S-expressions with source domain

---
+++ src/sl/sexpLoc.ml
@@ -0,0 +1,47 @@
+
+open Sexplib
+
+type domain =
+  | File of string
+
+type range = {
+  domain : domain;
+  start_pos : Sexp.Annotated.pos;
+  end_pos : Sexp.Annotated.pos;
+}
+
+type t =
+  | Atom of range * Type.t
+  | List of range * t list * Type.t
+
+let rec of_annotated domain = function
+  | Sexp.Annotated.Atom (range, t) ->
+    let range = {
+      domain;
+      start_pos = range.Sexp.Annotated.start_pos;
+      end_pos = range.Sexp.Annotated.end_pos;
+    } in
+    Atom (range, t)
+  | Sexp.Annotated.List (range, lst, t) ->
+    let range = {
+      domain;
+      start_pos = range.Sexp.Annotated.start_pos;
+      end_pos = range.Sexp.Annotated.end_pos;
+    } in
+    let lst = List.map (of_annotated domain) lst in
+    List (range, lst, t)
+    
+let rec to_annotated = function
+  | Atom (range, t) ->
+    let range = {
+      Sexp.Annotated.start_pos = range.start_pos;
+      Sexp.Annotated.end_pos = range.end_pos;
+    } in
+    Sexp.Annotated.Atom (range, t)
+  | List (range, lst, t) ->
+    let range = {
+      Sexp.Annotated.start_pos = range.start_pos;
+      Sexp.Annotated.end_pos = range.end_pos;
+    } in
+    let lst = List.map to_annotated lst in
+    Sexp.Annotated.List (range, lst, t)
