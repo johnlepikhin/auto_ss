8f5a296686b202114b5fc7dbafa8887648ceb9aa
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-01-28 00:45:22 +0300

    Initial SL macros support

---
+++ src/sl/slMacro.ml
@@ -0,0 +1,35 @@
+
+module M = Map.Make (struct type t = string let compare = compare end)
+
+type context = SexpLoc.t M.t
+
+let replace sl =
+  let open SexpLoc in
+  let open Sexplib in
+  let rec aux context = function
+    | [] -> []
+    | (Atom (_, Type.Atom name)) as atom :: tl ->
+      let newval =
+        try
+          M.find name context
+        with
+        | _ -> atom
+      in
+      newval :: aux context tl
+    | (List (_, [
+        Atom (_, Type.Atom "defmacro");
+        Atom (_, Type.Atom name);
+        List (_, args, _);
+        body
+      ], _)) :: tl ->
+      let context = M.add name body context in
+      aux context tl
+    | List (v1, lst, v2) :: tl ->
+      let lst = aux context lst in
+      List (v1, lst, v2) :: aux context tl
+    | (Atom _) as atom :: tl ->
+      atom :: aux context tl
+  in
+  match aux M.empty [sl] with
+  | [sl] -> sl
+  | _ -> sl
