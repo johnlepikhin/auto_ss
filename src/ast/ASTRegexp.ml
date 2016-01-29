88a4eac1e60c98b759328d81b7f7a6edcb354fa7
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-01-29 15:22:40 +0300

    Split huge regexps set into smaller acceptible by Pcre pieces

--- src/ast/ASTRegexp.ml
+++ src/ast/ASTRegexp.ml
@@ -9,27 +9,56 @@ type regexp = {
   id : int;
 }
 
-type smap = {
-  last_id : int;
+type superex = {
   map : regexp M.t;
   revmap : int RevM.t;
   superex : Pcre.regexp;
+  length : int;
 }
 
-type result = Superex.GroupsSet.t
+type smap = {
+  last_id : int;
+  set : superex list;
+}
+
+type result = Superex.GroupsSet.t list
+
+let rex_limit = 30000
+
+let initial_superex = {
+  map = M.empty;
+  revmap = RevM.empty;
+  superex = Pcre.regexp "";
+  length = 0;
+}
 
 (* Empty superex set *)
 let initial = {
   last_id = -1;
-  map = M.empty;
-  revmap = RevM.empty;
-  superex = Pcre.regexp "";
+  set = [];
 }
+
+let rec find_id rex = function
+  | [] -> raise Not_found
+  | superex :: tl ->
+    try
+      RevM.find rex superex.revmap
+    with
+    | Not_found ->
+      find_id rex tl
+
+let add_to_superex superex regexp flags =
+  {
+    superex with
+    map = M.add regexp.id regexp superex.map;
+    revmap = RevM.add (regexp.rex, flags) regexp.id superex.revmap;
+    length = superex.length + String.length regexp.rex;
+  }
 
 (* Add new regexp to set *)
 let register ~rex smap =
   try
-    let id = RevM.find rex smap.revmap in
+    let id = find_id rex smap.set in
     let (rex, flags) = rex in
     smap, { rex; flags; id }
   with
@@ -41,27 +70,45 @@ let register ~rex smap =
       flags;
       id = last_id;
     } in
-    let smap = {
-      smap with
-      last_id;
-      map = M.add last_id regexp smap.map;
-      revmap = RevM.add (rex, flags) last_id smap.revmap;
-    } in
-    smap, regexp
+    let set =
+      match smap.set with
+      | superex :: tl when superex.length + String.length rex < rex_limit ->
+        add_to_superex superex regexp flags :: tl
+      | superex :: tl ->
+        add_to_superex initial_superex regexp flags :: superex :: tl
+      | [] ->
+        [add_to_superex initial_superex regexp flags]
+    in
+    { set; last_id }, regexp
 
 (* Compile superex, make set applicable *)
-let compile smap =
-  let superex =
-    M.bindings smap.map
-    |> List.map (fun (key, regexp) -> (regexp.flags, regexp.rex))
-    |> Superex.superex
+let compile (smap : smap) =
+  let set =
+    List.map (fun s ->
+        let superex =
+          M.bindings s.map
+          |> List.map (fun (key, regexp) -> (regexp.flags, regexp.rex))
+          |> Superex.superex
+        in
+        { s with superex }
+      ) smap.set
   in
-  { smap with superex }
-
+  {
+    smap with set
+  }
+      
 (* Apply regexps set to string *)
-let apply smap s =
-  Superex.matched_regexps ~superex:smap.superex s
+let apply smap s : result =
+  List.map (fun superex -> Superex.matched_regexps ~superex:superex.superex s) smap.set
 
 (* Check if regexp (it's ID) matches *)
-let matches regexp groups_set =
-  Superex.GroupsSet.mem regexp.id groups_set
+let matches regexp (result : result) =
+  let rec aux = function
+    | [] -> false
+    | set :: tl ->
+      if Superex.GroupsSet.mem regexp.id set then
+        true
+      else
+        aux tl
+  in
+  aux result
