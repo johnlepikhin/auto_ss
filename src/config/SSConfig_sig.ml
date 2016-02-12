0f13b6977e152c24be917fcf879eeb4f33b01b89
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-02-12 15:02:26 +0300

    Add Virtual config domain

--- src/config/SSConfig_sig.ml
+++ src/config/SSConfig_sig.ml
@@ -2,10 +2,12 @@
 type domain =
   | File of string
   | Argument
+  | Virtual
 
 let string_of_domain = function
   | File s -> Printf.sprintf "file %s" s
   | Argument -> "command line argument"
+  | Virtual -> "virtual source"
 
 module type CONFIGREADER =
 sig
