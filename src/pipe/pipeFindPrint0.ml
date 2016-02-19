38601e564dec9a92838faa61aebb89be8b065b0e
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-02-20 01:24:58 +0300

    Refactoring to new pipes

--- src/pipe/pipeFindPrint0.ml
+++ src/pipe/pipeFindPrint0.ml
@@ -1,24 +1,25 @@
 
-open Pipe.Sig
-
-let file_of_string file = {
-  file;
-  tail = [];
-}
-
-let of_string s =
-  let open String in
-  let l = length s in
-  if l > 1 && s.[0] = '/' && s.[1] = '/' then
-    Meta (sub s 2 (l-2))
-  else
-    File (file_of_string s)
-
-let file_to_string r =
-  r.file
-
-let to_string = function
-  | File r -> file_to_string r
-  | Meta r -> r
-
-let record_separator = '\000'
+module Make (T : Pipe.TYPE) =
+struct
+  type record = T.record
+  type meta = T.meta
+  
+  let of_string s =
+    let open String in
+    let l = length s in
+    if l > 1 && s.[0] = '/' && s.[1] = '/' then
+      let line = sub s 2 (l-2) in
+      let meta = T.meta_of_line line in
+      Pipe.Meta meta
+    else
+      let record = T.record_of_fields [s] in
+      Pipe.Record record
+  
+  let to_string = function
+    | Pipe.Record r ->
+      T.fields_of_record r |> String.concat ""
+    | Pipe.Meta r ->
+      T.line_of_meta r
+  
+  let record_separator = '\000'
+end
