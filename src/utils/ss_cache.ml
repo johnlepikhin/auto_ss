d669a50d6e9a06b4cec18d0f9330c46385311384
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-03-07 10:35:19 +0300

    ss_cache: memory & CPU optimizations

--- src/utils/ss_cache.ml
+++ src/utils/ss_cache.ml
@@ -7,48 +7,76 @@ let args = ArgPipeFormat.argsInOut @ [
     "-l", Arg.Set_int limit, "Cache size (default 100000)";
   ]
 
-type r = {
-  mutable st : Unix.LargeFile.stats option;
-  mutable last : int64;
-}
-
 let () =
   Arg.parse args (fun _ -> ()) usage
 
-let hash : (string, r) Hashtbl.t = Hashtbl.create (!limit*2)
+type stat = {
+  mtime : float;
+  size : int64;
+}
 
-let cleanup () =
-  if Hashtbl.length hash > !limit then (
-    let keys = ref [] in
-    let cnt = ref 0 in
-    Hashtbl.iter (fun k v -> incr cnt; keys := (v.last, k) :: !keys) hash;
-    let keys = List.sort (fun (last1, _) (last2, _) -> compare last1 last2) !keys in
-    let rec skip n = function
-      | [] -> ()
-      | (_, k) :: tl ->
-        if n = 0 then ()
-        else (
-          Hashtbl.remove hash k;
-          skip (n-1) tl
-        )
-    in
-    skip (!cnt - !limit) keys
-  )
+type r = {
+  mutable st : stat option;
+  mutable last : int64;
+}
+
+module Key :
+sig
+  type t
+
+  val of_string: string -> t
+
+  val compare: t -> t -> int
+end =
+struct
+  type t = string
+
+  let of_string v =
+    let open Cryptokit in
+    let hash = Hash.sha1 () in
+    hash_string hash v
+
+  let compare = compare
+end
+
+let cache_preclean = Int64.of_int (!limit / 5)
+
+module Storage =
+struct
+  let hash : (Key.t, r) Hashtbl.t = Hashtbl.create (!limit)
+
+  let get key =
+    Hashtbl.find hash key
+    
+  let set key v =
+    Hashtbl.replace hash key v
+
+  let cleanup () =
+    let max_size = !limit - !limit / 5 in
+    let size = Hashtbl.length hash in
+    if size > max_size then (
+      let min_last = Hashtbl.fold (fun k { last } prev -> min prev last) hash Int64.max_int in
+      let delete_cnt = size - max_size in
+      let max_last = Int64.(add min_last (of_int delete_cnt)) in
+      Hashtbl.iter (fun k { last } -> if last < max_last then Hashtbl.remove hash k) hash;
+      Gc.full_major ()
+    )
+end
 
 let get_inc =
   let i = ref 0L in
   (* Cleanup cache every limit/5 cache updates *)
-  let cache_overhead = Int64.of_int (!limit / 5) in
+  let cache_cleanup_epoch = Int64.of_int (!limit / 5) in
   fun () ->
     i := Int64.succ !i;
-    if Int64.rem !i cache_overhead = 0L then
-      cleanup ();
+    if Int64.rem !i cache_cleanup_epoch = 0L then
+      Storage.cleanup ();
     !i
 
-let check_if_updated current file =
+let check_if_updated current key =
   let stored =
     try
-      Some (Hashtbl.find hash file)
+      Some (Storage.get key)
     with
     | _ -> None
   in
@@ -65,7 +93,13 @@ let check_if_updated current file =
   (* compare file info with cache record *)
   | Some current, Some { st = Some stored } ->
     let open Unix.LargeFile in
-    stored.st_mtime <> current.st_mtime || stored.st_size <> current.st_size
+    stored.mtime <> current.st_mtime || stored.size <> current.st_size
+
+let stat_of_unix_opt = function
+  | None -> None
+  | Some st ->
+    let open Unix.LargeFile in
+    Some { mtime = st.st_mtime; size = st.st_size }
 
 let () =
   Arg.parse args (fun _ -> ()) usage;
@@ -75,15 +109,17 @@ let () =
   let pipe = P.init stdin stdout in
   P.iter_input (function
       | Pipe.Record record -> (
+          let open Unix.LargeFile in
           let file = record.PipeFmtMain.Type.file in
+          let key = Key.of_string file in
           let current =
             try
-              Some (Unix.LargeFile.stat file)
+              Some (stat file)
             with
             | _ -> None
           in
-          let updated = check_if_updated current file in
-          Hashtbl.replace hash file { st = current; last = get_inc () };
+          let updated = check_if_updated current key in
+          Storage.set key { st = stat_of_unix_opt current; last = get_inc () };
           if updated then
             P.output pipe (Pipe.Record record);
         )
